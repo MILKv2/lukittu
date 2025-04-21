1c3e5806090fd767c20618522d2f8de000b21f99
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-04-21 13:11:26 +0300

    fix eslint config

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -2,32 +2,31 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  files: ['**/*.ts'],
-  ignores: [
-    '**/prisma/**',
-    '**/dist/**',
-    '**/*.spec.ts',
-    '**/*.test.ts',
-  ],
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
-    eslintConfigPrettier,
-    {
-      rules: {
-        'no-console': 'error',
-        '@typescript-eslint/no-non-null-assertion': 'off',
+export default tseslint.config(
+  {
+    ignores: ['**/prisma/**', 'dist/**', '**/*.spec.ts', '**/*.test.ts'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+      eslintConfigPrettier,
+      {
+        rules: {
+          'no-console': 'error',
+          '@typescript-eslint/no-non-null-assertion': 'off',
+        },
+      },
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-  ],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    parserOptions: {
-      project: ['./tsconfig.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
   },
-});
+);
