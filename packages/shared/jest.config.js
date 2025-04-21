20192b57abc763a428bb70c962000fa9ad9586fd
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-04-21 13:22:01 +0300

    ignore dist

--- packages/shared/jest.config.js
+++ packages/shared/jest.config.js
@@ -1,8 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    '^.+\.tsx?$': ['ts-jest', {}],
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
